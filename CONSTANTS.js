fe60bd6eedf61025b07f584da6a1f4618b8db8a1
i-sourabh <bsourabh191@gmail.com>
2021-08-11 10:57:53 +0530

    constants

---
+++ CONSTANTS.js
@@ -0,0 +1,73 @@
+exports.DHIS_URL_BASE = "https:
+
+
+exports.username = "";
+exports.password = "";
+exports.auth = "Basic " + new Buffer(exports.username + ":" + exports.password).toString("base64");
+exports.eventProgramStage = "iWcQ5lxmsy4";
+exports.eventProgram = "VjjfZbLMU9U";
+exports.eventDateKey = "Date";
+exports.eventUIDKey = "instanceID";
+exports.ODK_OrgUnitQuestion = "Address_Information/Village";
+exports.DHIS_DefaultCategoryCombo = "GLevLNI9wkl";
+exports.DHIS_ROOT_OU_UID = "OpEnoIFlcBH";
+exports.DHIS_DE_ODK_FORMID = "glBt6kk3AXS";
+exports.DHIS_DE_DUPLICATE_CASE = "DqQoNAJ3jwl";
+exports.DHIS_DE_DATE = "o5bAIpGOHqa";
+exports.DHIS_DE_MEMBEROFCLUSTER = "D6bx5aQ5DOo";
+exports.DHIS_DE_SYNDROME_UID = "ayM8O2WrO40";
+exports.DHIS_CLUSTER_TRACKED_ENTITY_UID = "tc9TjlFg8u5";
+exports.trackedEntity = "MCPQUTHX1Ze";
+exports.CLUSTER_TEA_LABCONFIRMED = "nMC9jWaMUTA";
+exports.CLUSTER_TEA_3AFICASE = "JaQY9q8sLSP";
+exports.CLUSTER_TEA_5AFICASE = "V9vUlm8MfCN";
+
+exports.CLUSTER_TEA_3ILICASE = "o7ozszF5igx";
+exports.CLUSTER_TEA_5ILICASE = "EWR0ft35ex9";
+exports.CLUSTER_TEA_2ADDCASE = "UmqB6qNqphk";
+exports.CLUSTER_TEA_IS_ACTIVE = "RAfdb1QLDkd";
+exports.CLUSTER_TEA_CLUSTERID = "yhTVy4gp1x0";
+
+exports.CLUSTER_TEA_CLUSTER_TYPE = "JWyErh3yii7";
+
+exports.CLUSTER_TEA_CASES_UIDS = "ZUYS7epFPnA";
+
+exports.CLUSTER_TEA_CLUSTER_METHOD = "GV6fosuimwJ";
+
+exports.CLUSTER_TEA_CLUSTER_INDEX_DATE = "ESHjGH529zp";
+
+exports.CLUSTER_TEA_CLUSTER_TAIL_DATE = "RLwAA4QvryL";
+
+exports.CLUSTER_TEA_CLUSTER_START_DATE = "IGzYEyItzOV";
+
+
+exports.CLUSTER_TEA__CLUSTER_IDENTIFIER = "eDCvZdpPzQV";
+
+exports.CLUSTER_TEA_COORDINATE = "JWyErh3yii7";
+
+exports.CLUSTER_TEA_FEATURETYPE = "yakZy1tzYiQ";
+
+exports.CLUSTER_PROGRAM = "i1saQy0blPh";
+
+exports.CLUSTER_PROGRAMSTAGE = "S8Fmja6YFf5";
+
+exports.CLUSTER_DE_CASE_TEI_UID = "lK8RnIkzGzJ";
+
+exports.CLUSTER_DE_GROUP_DE_TO_SHOW = "sXzZ0Xl2F0j";
+
+exports.LAB_DE_GROUP_UID = "YUK3LTTIakR";
+exports.EMAIL_GROUP_UID = "r9VXthBBBsk";
+exports.EMAIL_GROUP_CC_UID = "eS1b9n7YVc0";
+
+
+exports.DHIS_Registration_Program = "sIEPsgABx0L";
+exports.DHIS_Registration_ProgramStage = "jDDmmG7ZePy";
+exports.DHIS_Registration_Village_DE = "mEWZIvuQWwe";
+exports.DHIS_Registration_Origin_DE = "CPrDolo2qSh";
+exports.DHIS_Registration_Syndrome_DE = "xYO8HkDDW3N";
+exports.Reports = {
+    ClusterInformation: {
+        id: "DnjzL5wPJE5",
+        name: "Cluster_Information"
+    }
+}
