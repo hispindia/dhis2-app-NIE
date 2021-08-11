ae848f9243f826bec7fcb87b6b8c896d07f0804f
i-sourabh <bsourabh191@gmail.com>
2021-08-11 10:58:37 +0530

    events tei payload/api updates

---
+++ createTEIAndEvents.js
@@ -0,0 +1,275 @@
+module.exports = saveDataToTracker;
+
+var ajax = require("./ajax");
+var constant = require("./CONSTANTS");
+var utility = require('./utility-functions');
+var _ = require('underscore')._;
+var moment = require("moment");
+var merge = require('deepmerge');
+
+var eventUpdater = require('./eventUpdater');
+var format = "YYYY-MM-DD";
+
+function getClusterID(callback) {
+    ajax.getReq(constant.DHIS_URL_BASE + "/api/trackedEntityInstances?&page=1&totalPages=true&pageSize=1&program=" + constant.CLUSTER_PROGRAM + "&ouMode=DESCENDANTS&ou=" + constant.DHIS_ROOT_OU_UID, constant.auth, function (error, reponse, body) {
+        if (error) {
+            __logger.error("Get Cluster ID");
+        }
+        var body = JSON.parse(body);
+        callback(body.pager.total)
+    })
+}
+
+function saveDataToTracker(cluster_tei, clusterEvents, oneCaseEvent, clusterType, callback) {
+    __logger.info("Updated Cluster with UID ---" + cluster_tei)
+    getClusterID(function (clusterID) {
+
+        var endDate = new Date(cluster_tei.enrollments[0].enrollmentDate);
+        endDate.setDate(endDate.getDate() - 7);
+
+        cluster_tei.attributes.push({
+            "attribute": constant.CLUSTER_TEA_CLUSTER_TYPE,
+            "value": clusterType
+        })
+
+
+        cluster_tei.attributes.push({
+            "attribute": constant.CLUSTER_TEA_COORDINATE,
+            "value": JSON.stringify(oneCaseEvent.coordinate)
+        })
+
+        cluster_tei.attributes.push({
+            "attribute": constant.CLUSTER_TEA_FEATURETYPE,
+            "value": "POINT"
+        })
+        cluster_tei.attributes.push({
+            "attribute": constant.CLUSTER_TEA_IS_ACTIVE,
+            "value": "true"
+        })
+        cluster_tei.attributes.push({
+            "attribute": constant.CLUSTER_TEA_CLUSTER_METHOD,
+            "value": "FIXED"
+        })
+
+        var tailDate = utility.findValueAgainstId(cluster_tei.attributes, "attribute", constant.CLUSTER_TEA_CLUSTER_TAIL_DATE, "value");
+
+
+        /*    ajax.getReq(constant.DHIS_URL_BASE+"/api/trackedEntityInstances?filter="+constant.CLUSTER_TEA_CLUSTER_TAIL_DATE+":ge:"+moment(endDate).format(format)+"&ou="+oneCaseEvent.orgUnit+"&program="+constant.CLUSTER_PROGRAM +
+                        "&filter="+constant.CLUSTER_TEA_CLUSTER_TYPE+":eq:"+clusterType+
+                        "&filter="+constant.CLUSTER_TEA_IS_ACTIVE + ":eq:true"+
+                        "&filter="+constant.CLUSTER_TEA_CLUSTER_METHOD+":eq:FIXED",constant.auth,getTEI)
+          */
+        getTEI(null, null, '{"trackedEntityInstances" : []}');
+
+        function getTEI(error, response, body) {
+
+            if (error) {
+                __logger.error("fetch Cluster TEI");
+                return;
+            }
+
+            var response = JSON.parse(body);
+
+            if (response.trackedEntityInstances.length == 0) {
+
+                cluster_tei.attributes.push({
+                    "attribute": constant.CLUSTER_TEA_CLUSTERID,
+                    "value": "CLUSTER" + clusterID + "_" + cluster_tei.enrollments[0].enrollmentDate
+                })
+
+                makeNewTEI();
+
+            } else {
+
+                cluster_tei = doMerging(cluster_tei, getMaxTEI(response.trackedEntityInstances));
+
+                if (!cluster_tei) {
+                    __logger.debug("Update not needed; cases already present");
+                    callback();
+                    return;
+                }
+                ajax.putReq(constant.DHIS_URL_BASE + "/api/trackedEntityInstances/" + cluster_tei.trackedEntityInstance, cluster_tei, constant.auth, function (error, response, body) {
+                    if (error) {
+                        __logger.error("Update Cluster")
+                    }
+
+                    __logger.info("Updated Cluster with UID -" + body.response.reference)
+                    var clusterID = utility.findValueAgainstId(cluster_tei.attributes, "attribute", constant.CLUSTER_TEA_CLUSTERID, "value");
+
+
+                    var allCases = utility.findValueAgainstId(cluster_tei.attributes, "attribute", constant.CLUSTER_TEA_CASES_UIDS, "value");
+                    new eventUpdater(allCases, function () {
+                        callback();
+                    })
+
+                });
+
+            }
+
+        }
+
+        function getMaxTEI(teis) {
+            var maxDateTEI = teis[0];
+            for (var key in teis) {
+                var tailDateMax = utility.findValueAgainstId(maxDateTEI.attributes, "attribute", constant.CLUSTER_TEA_CLUSTER_TAIL_DATE, "value");
+                var tailDate = utility.findValueAgainstId(teis[key].attributes, "attribute", constant.CLUSTER_TEA_CLUSTER_TAIL_DATE, "value");
+                if (tailDateMax < tailDate) {
+                    maxDateTEI = teis[key];
+                }
+
+            }
+            return maxDateTEI
+
+        }
+
+        function doMerging(current, old) {
+
+            var totalAttr = []
+            var currentAttrMap = []
+            for (var i in current.attributes) {
+                currentAttrMap[current.attributes[i].attribute] = current.attributes[i].value;
+                totalAttr[current.attributes[i].attribute] = true;
+            }
+            var oldAttrMap = []
+            for (var i in old.attributes) {
+                oldAttrMap[old.attributes[i].attribute] = old.attributes[i].value;
+                totalAttr[old.attributes[i].attribute] = true;
+            }
+
+            for (var i in totalAttr) {
+
+            }
+
+            delete old.enrollments;
+            var new_casesUID = utility.findValueAgainstId(current.attributes, "attribute", constant.CLUSTER_TEA_CASES_UIDS, "value");
+            var old_casesUID = utility.findValueAgainstId(old.attributes, "attribute", constant.CLUSTER_TEA_CASES_UIDS, "value");
+
+            new_casesUID = new_casesUID.split(";");
+            if (!mergeIsNeeded(old_casesUID, new_casesUID)) {
+                return null;
+            }
+
+            old_casesUID = old_casesUID.split(";");
+
+
+            var mergedCases = [];
+            for (var key in new_casesUID) {
+                if (new_casesUID[key] == "") {
+                    continue
+                }
+                mergedCases[new_casesUID[key]] = true;
+            }
+
+            for (var key in old_casesUID) {
+                if (old_casesUID[key] == "") {
+                    continue
+                }
+                mergedCases[old_casesUID[key]] = true;
+            }
+            var mergedCaseUIDs = utility.reduceMapByKey(mergedCases, ";");
+            utility.putValueAgainstId(current.attributes, "attribute", constant.CLUSTER_TEA_CASES_UIDS, "value", mergedCaseUIDs);
+
+            for (var i = 0; i < current.attributes.length; i++) {
+
+                if (current.attributes[i].attribute == constant.CLUSTER_TEA_CLUSTER_INDEX_DATE) {
+                    continue
+                }
+                ;
+                if (current.attributes[i].attribute == constant.CLUSTER_TEA_CLUSTERID) {
+                    continue
+                }
+                ;
+                var attrObj = current.attributes[i];
+                var value = utility.findValueAgainstId(old.attributes, "attribute", attrObj.attribute, "value");
+                if (value) {
+                    utility.putValueAgainstId(old.attributes, "attribute", attrObj.attribute, "value", attrObj.value);
+                } else {
+                    old.attributes.push(attrObj);
+                }
+            }
+
+            oldAttrMap = []
+            for (var i in old.attributes) {
+                oldAttrMap[old.attributes[i].attribute] = old.attributes[i].value;
+                totalAttr[old.attributes[i].attribute] = true;
+            }
+            for (var i in totalAttr) {
+
+            }
+
+            return old;
+
+            function mergeIsNeeded(oldCasesString, newCasesList) {
+
+                for (var key in newCasesList) {
+                    var eventUID = newCasesList[key];
+
+                    if (oldCasesString.indexOf(eventUID) == -1) {
+                        return true
+                    }
+
+                }
+                return false;
+            }
+        }
+
+        function makeNewTEI() {
+
+            var oldAttrMap = [];
+            var totalAttr = [];
+            var old = cluster_tei;
+            for (var i in old.attributes) {
+                oldAttrMap[old.attributes[i].attribute] = old.attributes[i].value;
+                totalAttr[old.attributes[i].attribute] = true;
+            }
+            for (var i in totalAttr) {
+
+            }
+
+            var tailDate = utility.findValueAgainstId(cluster_tei.attributes, "attribute", constant.CLUSTER_TEA_CLUSTER_TAIL_DATE, "value");
+
+            cluster_tei.attributes.push({
+                "attribute": constant.CLUSTER_TEA_CLUSTER_START_DATE,
+                "value": tailDate
+            })
+
+            ajax.postReq(constant.DHIS_URL_BASE + "/api/trackedEntityInstances", cluster_tei, constant.auth, teiSave);
+
+
+            function teiSave(error, response, body) {
+                if (error) {
+                    __logger.error("POst Cluster TEI");
+                    return;
+                }
+
+                var clusterID = utility.findValueAgainstId(cluster_tei.attributes, "attribute", constant.CLUSTER_TEA_CLUSTERID, "value");
+                var indexCaseDate = utility.findValueAgainstId(cluster_tei.attributes, "attribute", constant.CLUSTER_TEA_CLUSTER_INDEX_DATE, "value");
+
+
+                __logger.info("Saved Cluster with UID -" + body.response.importSummaries[0].reference)
+                var indexEvent = {
+                    program: constant.CLUSTER_PROGRAM,
+                    programStage: constant.CLUSTER_PROGRAMSTAGE,
+                    orgUnit: cluster_tei.orgUnit,
+                    eventDate: indexCaseDate,
+                    trackedEntityInstance: cluster_tei.trackedEntityInstance
+                }
+
+                ajax.postReq(constant.DHIS_URL_BASE + "/api/events", indexEvent, constant.auth, function (error, response, body) {
+                    if (error) {
+                        __logger.error("Error saving index Event");
+                    }
+
+                });
+
+                var allCases = utility.findValueAgainstId(cluster_tei.attributes, "attribute", constant.CLUSTER_TEA_CASES_UIDS, "value");
+                new eventUpdater(allCases, function () {
+                    callback();
+                })
+
+            }
+
+
+        }
+    })
+}
---
+++ updateEvent.js
@@ -0,0 +1,96 @@
+
+var ajax = require("./ajax");
+var constant=require("./CONSTANTS");
+var utility = require('./utility-functions');
+var _ = require('underscore')._;
+var moment = require("moment");
+var request = require('request');
+
+
+this.getRecentEventDate = function(callback){
+
+    ajax.getReq(constant.DHIS_URL_BASE+"/api/events?orgUnit="+constant.DHIS_ROOT_OU_UID+"&pageSize=2&ouMode=DESCENDANTS&order=eventDate:DESC&program="+constant.CLUSTER_PROGRAM+"&totalPages=true",constant.auth,
+                function(error,response,body){
+                  
+                    callback(error,response,body)
+                    
+                });
+    
+    
+}
+
+this.updateDate = function(){
+
+    var events = [
+        "Of65a7ce7cd",
+        "Oe8d9b3f1ce",      
+        "O52c866ec53"
+    ]
+
+    getClusterCases(events,function(events){
+        var fixedAlgoCases = filterEventsByDataValue(events,"dataElement",constant.DHIS_DE_ODK_FORMID,"value",["DPHL_Lab_V1","eDFSS_IPD_V3"],"include");  
+
+        updateDate(0,fixedAlgoCases)
+    });
+
+  function getClusterCases(cases,callback){
+        
+
+        var clusterCases = [];
+        getEvent(0,cases);
+        function getEvent(index,cases){
+            if (index == cases.length){
+                callback(clusterCases);
+                return
+            }
+
+            ajax.getReq(constant.DHIS_URL_BASE+"/api/events/"+cases[index],constant.auth,
+                        function(error,response,body){
+                            if (error){
+                                console.log("Error Fetch Event")
+                            }
+                            clusterCases.push(JSON.parse(body));
+                            getEvent(index+1,cases);
+                        });
+        }   
+      
+  }
+
+    function updateDate(index,events){
+        
+        if (index == events.length){return}
+        var event = events[index];
+
+        var date = utility.findValueAgainstId(event.dataValues,"dataElement",constant.DHIS_DE_DATE,"value");
+        if (!date){
+            event.dataValues.push({
+                dataElement : constant.DHIS_DE_DATE,
+                value : moment(event.eventDate).format("YYYY-MM-DD")
+            })
+        }
+
+        
+        
+        ajax.putReq(constant.DHIS_URL_BASE+"/api/events/"+event.event,event,constant.auth,function(error,response,body){
+            if (error){
+                console.log("Error updating event")
+            }
+            
+            updateDate(index+1,events);
+        })
+        
+    }
+}
+
+  function filterEventsByDataValue(events,idKey,id,valKey,values,operation){
+        var list = [];
+        for (var i=0;i<events.length;i++){
+            if (utility.checkListForValue(events[i].dataValues,idKey,id,valKey,values)){
+                if (operation == "include")  list.push(events[i]);
+            }else{
+                if (operation == "exclude")  list.push(events[i]);
+            }
+        }
+        return list;
+    }
+
